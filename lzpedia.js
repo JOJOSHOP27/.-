b160209474ef86c84bbcc8a7506b8cdf5f0f9db9
JOJOSHOP27 <virtusim27@gmail.com>
2026-08-14 10:36:10 +0800

    Update lzpedia.js

--- lzpedia.js
+++ lzpedia.js
@@ -1,11 +1,8 @@
 // ============================================================
 // LZPEDIA API - VERCEL SERVERLESS FUNCTION
-// Endpoint: /api/lzpedia?action=create&amount=2000
-//           /api/lzpedia?action=status&invoice_id=INVxxx
 // ============================================================
 
 export default async function handler(req, res) {
-    // CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
